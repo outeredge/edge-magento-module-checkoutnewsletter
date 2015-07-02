7fc83bdb74f378216288d21c5baabd4137e4c7fa
Michael Windell <mike@outeredgeuk.com>
2015-07-02 11:43:58 +0100

    SagePay FIX checkout/session to checkout/type_onepage->getCheckout()

--- app/code/local/Edge/CheckoutNewsletter/Model/Newsletter.php
+++ app/code/local/Edge/CheckoutNewsletter/Model/Newsletter.php
@@ -7,16 +7,18 @@ class Edge_CheckoutNewsletter_Model_Newsletter
         $controllerAction = $observer->getEvent()->getControllerAction();
         if ($controllerAction) {
             $newsletterCheckbox = $controllerAction->getRequest()->getPost('subscribe_to_newsletter');
-            Mage::getModel('core/session')->setNewsletterChecked((bool)$newsletterCheckbox);
+            Mage::getModel('checkout/type_onepage')->getCheckout()->setNewsletterChecked((bool)$newsletterCheckbox);
         }
     }
 
     public function subscribeToNewsletter(Varien_Event_Observer $observer)
     {
         $quote = $observer->getEvent()->getQuote();
-        if (in_array($quote->getCheckoutMethod(), array('login_in', 'guest')) && Mage::getModel('core/session')->getNewsletterChecked()) {
+        if (in_array($quote->getCheckoutMethod(), array('login_in', 'guest')) &&
+            Mage::getModel('checkout/type_onepage')->getCheckout()->getNewsletterChecked())
+        {
             Mage::getModel('newsletter/subscriber')->subscribe($quote->getBillingAddress()->getEmail());
-            Mage::getModel('core/session')->unsNewsletterChecked();
+            Mage::getModel('checkout/type_onepage')->getCheckout()->unsNewsletterChecked();
         }
     }
 }
